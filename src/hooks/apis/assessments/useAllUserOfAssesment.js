63ca1c07bf84a8b974874d9d415da4c6f5dd69f4
Abhijit Dinda <75515614+AbhijitDinda@users.noreply.github.com>
2025-01-25 13:46:07 +0530

    updatee

--- src/hooks/apis/assessments/useAllUserOfAssesment.js
+++ src/hooks/apis/assessments/useAllUserOfAssesment.js
@@ -1,21 +1,29 @@
-import {useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { getAssessmentsAllUser } from '@/apis/assesments';
 import { useAuth } from "@/hooks/context/uesAuth";
 
 export const useAllUserOfAssesment = (assessment_id) => {
-    const {auth,logout} = useAuth();
+    const { auth, logout } = useAuth();
 
-    const {isFetching, isLoading, isError ,isSuccess,error,data:userOfAssesments } = useQuery({
-        queryFn: () => getAssessmentsAllUser({ token: auth?.token ,logout,assessment_id}),
-        queryKey: ['getUserOfAssesments',assessment_id]
-    })
+    const {
+        isFetching,
+        isLoading,
+        isError,
+        isSuccess,
+        error,
+        data: userOfAssesments,
+    } = useQuery({
+        queryFn: () => getAssessmentsAllUser({ token: auth?.token, logout, assessment_id }),
+        queryKey: ['getUserOfAssesments', assessment_id],
+        enabled: !!assessment_id, // Fetch only if `assessment_id` is valid
+    });
 
-    return{
+    return {
         isFetching,
         isLoading,
         isSuccess,
         isError,
         error,
-        userOfAssesments
-    }
+        userOfAssesments,
+    };
 };
--- src/pages/assessments/action/[slug].jsx
+++ src/pages/assessments/action/[slug].jsx
@@ -3,10 +3,14 @@ import AssessmentsActionTabs from "@/components/Assessments/AssessmentsAction/As
 import Heading from "@/components/Heading";
 import { useAllUserOfAssesment } from "@/hooks/apis/assessments/useAllUserOfAssesment";
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function AssessmentsAction() {
     const params = useParams()
-  const {isLoading,userOfAssesments} = useAllUserOfAssesment(params?.slug || null);
+    const assessmentId = params?.slug;
+
+
+  const {isLoading,userOfAssesments} = useAllUserOfAssesment(assessmentId);
 
   return (
     <div className="rounded-sm mx-auto w-full max-w-[1300px]">
