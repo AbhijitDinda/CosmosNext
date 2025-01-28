81d6370bfa1179d111d788c068b69ef034b972c9
Abhijit Dinda <75515614+AbhijitDinda@users.noreply.github.com>
2025-01-28 19:29:27 +0530

    handle Error in wrong param pass in browser

--- src/pages/assessments/action/[slug].jsx
+++ src/pages/assessments/action/[slug].jsx
@@ -6,22 +6,30 @@ import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function AssessmentsAction() {
-    const params = useParams()
-    const assessmentId = params?.slug;
+  const params = useParams()
+  const assessmentId = params?.slug;
 
-  const {isLoading,userOfAssesments} = useAllUserOfAssesment(assessmentId);
-  
-  // console.log("userOfAssesments 1",userOfAssesments?.data?.data?.test_token)
+  const { isLoading, isSuccess, isError, error, userOfAssesments } = useAllUserOfAssesment(assessmentId);
 
+  console.log(isError)
 
   return (
+
     <div className="rounded-sm mx-auto w-full max-w-[1300px]">
       <Heading title={`${userOfAssesments?.data?.data.test_name} Assesment`} />
       <div className="p-4 bg-White rounded-sm">
-        <AssessmentFilterAndAnalytics data={userOfAssesments?.data?.data} isLoading={isLoading} />
-        
-        <AssessmentsActionTabs data={userOfAssesments?.data?.data?.users} token={userOfAssesments?.data?.data?.test_token} isLoading={isLoading} />
+
+        {(isError) ? (
+          <div>Something went wrong</div>
+        ) : (
+          <>
+            <AssessmentFilterAndAnalytics data={userOfAssesments?.data?.data} isLoading={isLoading} />
+            <AssessmentsActionTabs data={userOfAssesments?.data?.data?.users} token={userOfAssesments?.data?.data?.test_token} isLoading={isLoading} />
+          </>
+        )}
       </div>
     </div>
-  );
+  )
+
+
 }
