03e560334ea7d306d57c8a74ab6a68599b2a90f2
Purbarun <purbarunmondal@gmail.com>
2025-02-10 13:37:47 +0530

    style: update global styles for consistency and add list styling

--- src/components/Candidate/CandidateAction/CandidateActionTabs/components/CompetenceMotivation.jsx
+++ src/components/Candidate/CandidateAction/CandidateActionTabs/components/CompetenceMotivation.jsx
@@ -1,24 +1,41 @@
-const CompetenceMotivation = ({ title, type, description ,behaviors ,challenges}) => {
-    return (
-        <div className="w-full md:w-4/6 mx-auto">
-            <h2 className="text-2xl font-bold text-gray-800">{title}</h2>
-            <p className="text-gray-700 font-semibold mt-2">
-                <span className="font-bold">Type:</span> {type}
-            </p>
+const CompetenceMotivation = ({
+  title,
+  type,
+  description,
+  behaviors,
+  challenges,
+}) => {
+  return (
+    <div className="w-full md:w-4/6 mx-auto">
+      <h2 className="text-2xl font-bold text-gray-800">{title}</h2>
+      <p className="text-gray-700 font-semibold mt-2">
+        <span className="font-bold">Type:</span> {type}
+      </p>
 
-            <p className="text-gray-700 mt-4" dangerouslySetInnerHTML={{ __html:description}} />
+      <div
+        className="text-gray-700 mt-4"
+        dangerouslySetInnerHTML={{ __html: description }}
+      />
 
-            <p className="font-semibold pt-8">Common behaviours associated with Competence motivation include: </p>
+      <p className="font-semibold pt-8">
+        Common behaviours associated with Competence motivation include:{" "}
+      </p>
 
-            <p className="text-gray-700 mt-4" dangerouslySetInnerHTML={{ __html:behaviors}} />
-            
-            <p className="font-semibold pt-8">Challenges with employees driven by Competence Motivation:</p>
+      <p
+        className="text-gray-700 mt-4"
+        dangerouslySetInnerHTML={{ __html: behaviors }}
+      />
 
-            <p className="text-gray-700 mt-4" dangerouslySetInnerHTML={{ __html:challenges}} />
+      <p className="font-semibold pt-8">
+        Challenges with employees driven by Competence Motivation:
+      </p>
 
-
-        </div>
-    );
+      <p
+        className="text-gray-700 mt-4"
+        dangerouslySetInnerHTML={{ __html: challenges }}
+      />
+    </div>
+  );
 };
 
 export default CompetenceMotivation;
